37eb97d74d80873528a1309146230fa3d6abd390
aynurahmed500-pixel <aynurahmed500@gmail.com>
2026-06-24 17:07:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function toggleMenu() {
   var menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
+
+
+<script>
+let zoom = 1;
+
+function zoomIn() {
+  zoom += 0.1;
+  document.getElementById("mapWrapper").style.transform = `scale(${zoom})`;
+}
+
+function zoomOut() {
+  zoom = Math.max(0.5, zoom - 0.1);
+  document.getElementById("mapWrapper").style.transform = `scale(${zoom})`;
+}
+</script>
