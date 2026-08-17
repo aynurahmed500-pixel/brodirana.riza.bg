e841ade57fb7dfeda74cc31afce0d5b7de14ac65
aynurahmed500-pixel <aynurahmed500@gmail.com>
2026-08-17 15:51:42 +0300

    Remove zoomIn and zoomOut functions from index.js
    Removed zoom functionality and related script from index.js.

--- index.js
+++ index.js
@@ -4,17 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("active");
 }
 
-
-<script>
-let zoom = 1;
-
-function zoomIn() {
-  zoom += 0.1;
-  document.getElementById("mapWrapper").style.transform = `scale(${zoom})`;
-}
-
-function zoomOut() {
-  zoom = Math.max(0.5, zoom - 0.1);
-  document.getElementById("mapWrapper").style.transform = `scale(${zoom})`;
-}
-</script>
